8e54f00a4942bfefd63a5de3d91fdc2ae509ed40
Michal Palinski <m.k.palinski@gmail.com>
2022-03-28 17:41:14 +0200

    resize

--- assets/resizing.js
+++ assets/resizing.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', () => {
 
-            parent.postMessage(JSON.stringify({ event_id: 'handshake', data: { height: document.body.scrollHeight } }), '*');
+            parent.postMessage(JSON.stringify({ event_id: 'handshake', data: { height: document.querySelector('#dash').scrollHeight } }), '*');
 
             function onResize() {
-                parent.postMessage(JSON.stringify({ event_id: 'resized', data: { height: document.body.scrollHeight } }), '*');
+                parent.postMessage(JSON.stringify({ event_id: 'resized', data: { height: document.querySelector('#dash').scrollHeight } }), '*');
             }
 
             window.addEventListener('resize', onResize, false);
